f8af38e4c3311631b01c82d689acd4468b2bf108
ShreyaDayma-cse <shreyadayma0901@gmail.com>
2022-07-31 18:20:36 +0530

    test4

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import './App.css';
 import authApi from './authApi';
 import {useNavigate} from 'react-router-dom'
 import { useState } from 'react';
+import tokenApi from './tokenApi';
 
  
 function App() {
@@ -24,33 +25,7 @@ function App() {
           token_code = match;
       });
   }
-  function tokenApi(token_code:any){
-    const client_id = "4faa6988bb62a5d866f5d9d8778f0b1bda7214ec4c87da4c50d26058f2c5736a";
-    const client_secret = "ad81881a417c790cad0c3c1ee4c0655de2b5849e3f247b0d0d8a7048ffe62740";
-    const redirect_uri = "(https://squid-app-yfz8f.ondigitalocean.app/";
-    const code = token_code;
-    
-
-    // console.log("Code: "+code); 
-
-    fetch(`https://cloud.digitalocean.com/v1/oauth/token?client_id=${client_id}&client_secret=${client_secret}&code=${code}&grant_type=authorization_code&redirect_uri=${redirect_uri}`, {
-        "method": "POST"
-    })
-    
-    .then(response => response.json())
-    .then(response => {
-        console.log(response);
-       if(response)
-        setCheck(true)
-        console.log(check);
-        
-    })
-    .catch(err => { 
-
-        console.log(err);
-    });
-    
-}
+ 
   
 
 
@@ -61,7 +36,7 @@ function App() {
         {
           token_code === "" 
           ?<button className="btn btn-primary" type='button' onClick={() => authApi()}>Authenticate</button>
-          :<button className="btn btn-primary" type='button' onClick={() => tokenApi(token_code)}>Get Token</button>
+          :<button className="btn btn-primary" type='button' onClick={() => tokenApi(token_code,setCheck,check)}>Get Token</button>
           
         }
          { check&&<div>
