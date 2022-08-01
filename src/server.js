c1d9d785fc8df1201d567cb22d2d2b4f67b5e00e
ShreyaDayma-cse <shreyadayma0901@gmail.com>
2022-08-01 16:31:43 +0530

    test

--- src/server.js
+++ src/server.js
@@ -6,12 +6,43 @@ const multerS3 = require('multer-s3');
 
 const app = express();
 
+// Set S3 endpoint to DigitalOcean Spaces
+const spacesEndpoint = new aws.Endpoint('fra1.digitaloceanspaces.com');
+const s3 = new aws.S3({
+  endpoint: spacesEndpoint
+});
 
-// Views in public directory
+// Change bucket property to your Space name
+const upload = multer({
+  storage: multerS3({
+    s3: s3,
+    bucket: 'your-space-here',
+    acl: 'public-read',
+    key: function (request, file, cb) {
+      console.log(file);
+      cb(null, file.originalname);
+    }
+  })
+}).array('upload', 1);
+
 app.use(express.static('src'));
 app.get('/', function (request, response) {
     response.sendFile(__dirname + '/src/App.tsx');
   });
+  
+app.post('/upload', function (request, response, next) {
+    upload(request, response, function (error) {
+      if (error) {
+        console.log(error);
+        return response.redirect("/error");
+      }
+      console.log('File uploaded successfully.');
+      response.redirect("/success");
+    });
+  });
+
+// Views in public directory
+
 
 app.listen(3000, function () {
   console.log('Server listening on port 3000.');
