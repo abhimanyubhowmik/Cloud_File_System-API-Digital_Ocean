895da054d8052fd6fc1da9a8bb75b1b236c5d524
ShreyaDayma-cse <shreyadayma0901@gmail.com>
2022-08-16 10:41:52 +0530

    test1

--- src/download_file.js
+++ src/download_file.js
@@ -28,11 +28,13 @@ const bucketParams = {
   //     stream.on('end', () => resolve(Buffer.concat(chunks).toString('utf8')));
   //   });
   // };
-  var message = "My String"
+  var message = "My String";
+
   toString(response.Body).then(function (msg) {
     console.log(msg);
     message=msg;
   })
+  
   try {
     const response = await s3Client.send(new GetObjectCommand(bucketParams));
     console.log("Inside try block1");
