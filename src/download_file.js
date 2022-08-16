f158d7f41629e0f43d06649ab9d16755dd679c9d
ShreyaDayma-cse <shreyadayma0901@gmail.com>
2022-08-16 10:18:03 +0530

    test

--- src/download_file.js
+++ src/download_file.js
@@ -30,7 +30,10 @@ const bucketParams = {
   
   try {
     const response = await s3Client.send(new GetObjectCommand(bucketParams));
-    console.log(response.Body)
+    console.log("Inside try block1");
+    console.log(response.Body);
+    console.log("Inside try block2");
+    
     //const data = await streamToString(response.Body);
     //writeFileSync("downloaded-file.txt", data);
     //console.log("Success", data);
